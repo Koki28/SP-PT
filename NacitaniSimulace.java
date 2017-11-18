86e37dfe4da1f28e0bccd04cba1b3666453d8b28
Iopi <33093127+Iopi@users.noreply.github.com>
2017-11-18 16:51:40 +0100

    prechod2
    myslim ze tam je par chyb (treba u stacku pri tom breaku.. njeaka logicka chyba).. mohl bys to postupne zkontrolovat a pripadne i opravit.
    Do souboru se to nevypisuje spravne.. nejak na preskacku nevim proc.. kdyztak to zmen

--- NacitaniSimulace.java
+++ NacitaniSimulace.java
@@ -1,12 +1,13 @@
 package sp;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -18,8 +19,7 @@ import java.util.Scanner;
 public class NacitaniSimulace {
 
 	private static Scanner sc = new Scanner(System.in);
-	//private static Random r = new Random();
-	public static ArrayList<LinkedList> dijkstri = new ArrayList<LinkedList>();
+
 	private static int ztratovostReseni = 0;
 
 	/**
@@ -35,8 +35,6 @@ public class NacitaniSimulace {
 		System.out.println("Napište název souboru simulace: ");
 		String simulace = sc.next();
 		try (BufferedReader br = new BufferedReader(new FileReader(simulace))) {
-
-			//	try(BufferedReader br = new BufferedReader(new FileReader("simulaceTest.txt"))) {
 
 			String radkaSimulace;
 
@@ -69,96 +67,44 @@ public class NacitaniSimulace {
 				cesta.zpracujVrchol(zdrojV);
 				LinkedList <Vrchol> dijkstra = cesta.getCesta(cilV);
 
-				if(dijkstra == null)
+				if(dijkstra == null) {
 					System.out.println("Cesta neexistuje.");
-				else
-					System.out.println("Cesta z vrcholu " + zdroj + " do vrcholu " + cil  + ": ");
-				for(Vrchol vrchol : dijkstra) {
-
-					System.out.println(vrchol);
-				} 
-				dijkstri.add(dijkstra);
-				
-				PrechodDat prechod = new PrechodDat();
-				System.out.println("\n<-------------------------------------->");
-				prechod.posliData(cas-1, data, dijkstra);
-				ztratovostReseni += prechod.vypisZtratovost();
-				System.out.println("<-------------------------------------->");
-				
-				
-				
-				/*System.out.println("<-------------------------------------->");
-				int krok = 0;
-				for(int i = 0; i < dijkstra.size()-1; i++) {
-					krok++;
-								
-					Vrchol prvni = dijkstra.get(i); 
-					Vrchol druhy = dijkstra.get(i+1); 
-					System.out.println("\n--- krok " + krok + " ---");
-					System.out.println("Posílám data z vrcholu " +  prvni.getId() + " do vrcholu " + druhy.getId() + ": ");
-					
-					Hrana hrana = graf.getHrana(prvni, druhy);
-					int propustnostHrany = hrana.getPropustnost(); 	
-					double chybovostHrany = hrana.getChybovost();
-					double rand = r.nextDouble();
-					
-					System.out.println("propustnost " + prvni.getId() + "->" + druhy.getId() + ": " + propustnostHrany);
-					System.out.println("chybovost " + prvni.getId() + "->" + druhy.getId() + ": " + chybovostHrany);
-					
-					
-					if(propustnostHrany > data) {
-						//if (((double)data / (double)propustnostHrany) > chybovostHrany) 
-						//	if (rand < 0.5);
-						//		//posli znova pulku dat
-						
-						System.out.println("Z vrcholu " + prvni.getId() + " do vrcholu " + druhy.getId() + " se poslala všechna data(" + data + ").");
-						//poslat data
-						
-					}
-					
-					else{
-						int pozastavenaData = data - propustnostHrany;
-					 	//rand = r.nextDouble();
-						//if (rand < 0.5);
-						//	//posli znova pulku dat
-						System.out.println("Z vrcholu " + prvni.getId() + " do vrcholu " + druhy.getId() + " bylo posláno " + propustnostHrany + " dat.");
-						//poslat data
-						prvni.stack.pridatPamet(pozastavenaData);
-						System.out.println(pozastavenaData + " dat bylo uloženo do stacku vrcholu " + prvni.getId() + ".");
-						
-						while(pozastavenaData != 0) {
-						krok++;
-						System.out.println("\n--- krok " + krok + " ---");
-						
-						if (propustnostHrany > pozastavenaData) {
-							//if (((double)pozastavenaData / (double)propustnostHrany) > chybovostHrany) 
-							//if (rand < 0.5);
-							//	//posli znova pulku dat
-							System.out.println("Z vrcholu " + prvni.getId() + " do vrcholu " + druhy.getId() + " se poslal zbytek dat(" + pozastavenaData + ").");
-							//poslat data
-							prvni.stack.smazatData(pozastavenaData);
-							System.out.println(pozastavenaData + " dat bylo smazáno ze stacku vrcholu " + prvni.getId() + ".");
-							break;
-						}
-						
-						//rand = r.nextDouble();
-						//if (rand < 0.5);
-						//	//posli znova pulku dat
-						System.out.println("Z vrcholu " + prvni.getId() + " do vrcholu " + druhy.getId() + " boly posláno " + propustnostHrany + " dat.");
-						//poslat data
-						prvni.stack.smazatData(propustnostHrany);
-						System.out.println(propustnostHrany + " dat bylo smazáno ze stacku vrcholu " + prvni.getId() + ".");
-						pozastavenaData -= propustnostHrany;	
-						}
-						
-					}
-					
+					return;
 				}
-				System.out.println("<-------------------------------------->");*/
-				
+
+				try (BufferedWriter bw = new BufferedWriter(new FileWriter("prubehSimulace.txt", true))) {
+					System.out.println("Cesta z vrcholu " + zdroj + " do vrcholu " + cil  + ": ");
+					
+					bw.write("Cesta z vrcholu " + zdroj + " do vrcholu " + cil  + ": ");
+					bw.newLine();
+					for(Vrchol vrchol : dijkstra) {
+
+						System.out.println(vrchol);
+						bw.write("->" + vrchol);
+						bw.newLine();
+					} 
+
+					PrechodDat prechod = new PrechodDat();
+					System.out.println("\n<-------------------------------------->");
+					bw.write("<-------------------------------------->");
+					bw.newLine();
+					prechod.posliData(cas-1, data, dijkstra);
+					ztratovostReseni += prechod.vypisZtratovost();
+					System.out.println("<-------------------------------------->");
+					bw.write("<-------------------------------------->");
+					bw.newLine();
+					
+					bw.close();
+				}
+
+				catch (Exception e) {
+
+					System.err.println("Nepodaøilo se zapsat do souboru 'vstupTest.txt'.");
+				}
+
 			}
 
-		/*	System.out.println();
+			/*	System.out.println();
 
 			cesta.zpracujVrchol(graf.getVrchol(1));
 			LinkedList <Vrchol> dijkstra = cesta.getCesta(graf.getVrchol(10));
@@ -181,13 +127,4 @@ public class NacitaniSimulace {
 		}
 	}
 
-/*	/**
-	* Vrací ArrayList naplnìný seznamem nejkratších cest.
-	* 
-	* @return  Vytvoøený ArrayList.
-	*/
-	/*public static ArrayList<LinkedList> getDijkstri() {
-		// TODO Auto-generated method stub
-		return dijkstri;
-	}*/
 }
--- PrechodDat.java
+++ PrechodDat.java
@@ -1,6 +1,7 @@
 package sp;
 
-//import java.util.ArrayList;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.util.LinkedList;
 import java.util.Random;
 
@@ -8,151 +9,206 @@ public class PrechodDat {
 
 	private static Random r = new Random();
 
-	//public ArrayList<LinkedList> dijstri = NacitaniSimulace.getDijkstri();
 	Graf graf = NacitaniVstupnihoSouboru.getGraf();
-	
-	int ztratovost = 0;
-	
-	//int cas;
-	//int data;
-	//LinkedList<Vrchol> dijkstra;
-	
-	public PrechodDat() {
 
-	//	this.cas = cas;
-	//	this.data = data;
-	//	this.dijkstra = dijkstra;
+
+	int ztratovost = 0;
+
+	public PrechodDat() {
 	}
-	
+
 	public void posliData(int cas, int data, LinkedList<Vrchol> dijkstra) {
 		int ubytek = 0;
 		int poslano = 0;
 		int vterina = cas;
 		double rand;
-		
-		for(int i = 0; i < dijkstra.size()-1; i++) {
-			vterina++;
-			Vrchol prvni = dijkstra.get(i); 
-			Vrchol druhy = dijkstra.get(i+1); 
-			System.out.println("\n--- vterina " + vterina + " ---");
-			System.out.println("Posílám data o velikosti <" + data + "> z vrcholu " +  prvni.getId() + " do vrcholu " + druhy.getId() + ": ");
-			
-			Hrana hrana = graf.getHrana(prvni, druhy);
-			int propustnostHrany = hrana.getPropustnost(); 	
-			double chybovostHrany = hrana.getChybovost();
-			
-			System.out.println("propustnost " + prvni.getId() + "->" + druhy.getId() + ": " + propustnostHrany);
-			System.out.println("chybovost " + prvni.getId() + "->" + druhy.getId() + ": " + chybovostHrany);
-			
-			
-			if(propustnostHrany > data) {
-				rand = r.nextDouble();
-				
-				System.out.println(rand);
-				
-				if (((double)data / (double)propustnostHrany) > chybovostHrany) 
-					if (rand < 0.5) {
-						ubytek = (int)Math.ceil(data/2.0);
-						System.out.println("-Pøetížení!-  Pùlka dat se musí poslat znovu (" + ubytek + ").");
-						ztratovost += ubytek;
-						posliData(vterina, ubytek, dijkstra);
-						
-						System.out.println("\n--- vterina " + vterina + " ---");
-						data = data - ubytek;
-						
-					}
-				
-				System.out.println("Z vrcholu " + prvni.getId() + " do vrcholu " + druhy.getId() + " bylo posláno " + data + " dat.");
-				//poslat data dal
-				
-			}
-			
-			else{
-				int pozastavenaData = data - propustnostHrany;
-				poslano = propustnostHrany;
-			 	rand = r.nextDouble();
-			 	
-			 	System.out.println(rand);
-			 	if (chybovostHrany != 1.0)
-				if (rand < 0.5) {
-					ubytek = (int)Math.ceil(propustnostHrany/2.0); 
-					System.out.println("-Pøetížení!-  Pùlka dat se musí poslat znovu (" + ubytek + ").");
-					ztratovost += ubytek;
-					posliData(vterina, ubytek, dijkstra);
-					
-					System.out.println("\n--- vterina " + vterina + " ---");
-					poslano = propustnostHrany - ubytek;
-				}
-				System.out.println("Z vrcholu " + prvni.getId() + " do vrcholu " + druhy.getId() + " bylo posláno " + poslano + " dat.");
-				//poslat data
-				if (!prvni.stack.pridatPamet(pozastavenaData)) {
-					System.out.println("Pamì vrcholu " + prvni.getId() + " pøetekla! Data se musí poslat znovu (" +  pozastavenaData + ").");
-					posliData(vterina, pozastavenaData, dijkstra);
-					break;
-				}
-				else
-				System.out.println(pozastavenaData + " dat bylo uloženo do stacku vrcholu " + prvni.getId() + ".");
-				
-				while(pozastavenaData != 0) {
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter("prubehSimulace.txt", true))) {
+
+
+			for(int i = 0; i < dijkstra.size()-1; i++) {
 				vterina++;
+				Vrchol prvni = dijkstra.get(i); 
+				Vrchol druhy = dijkstra.get(i+1); 
 				System.out.println("\n--- vterina " + vterina + " ---");
-				
-				if (propustnostHrany > pozastavenaData) {
-					poslano = pozastavenaData;
+				System.out.println("Posílám data o velikosti <" + data + "> z vrcholu " +  prvni.getId() + " do vrcholu " + druhy.getId() + ": ");
+				bw.newLine();
+				bw.newLine();
+				bw.write("--- vterina " + vterina + " ---");
+				bw.newLine();
+				bw.write("Posílám data o velikosti <" + data + "> z vrcholu " +  prvni.getId() + " do vrcholu " + druhy.getId() + ": ");
+				bw.newLine();
+
+				Hrana hrana = graf.getHrana(prvni, druhy);
+				int propustnostHrany = hrana.getPropustnost(); 	
+				double chybovostHrany = hrana.getChybovost();
+
+				System.out.println("propustnost " + prvni.getId() + "->" + druhy.getId() + ": " + propustnostHrany);
+				System.out.println("chybovost " + prvni.getId() + "->" + druhy.getId() + ": " + chybovostHrany);
+				bw.write("propustnost " + prvni.getId() + "->" + druhy.getId() + ": " + propustnostHrany);
+				bw.newLine();
+				bw.write("chybovost " + prvni.getId() + "->" + druhy.getId() + ": " + chybovostHrany);
+				bw.newLine();
+
+
+				if(propustnostHrany > data) {
 					rand = r.nextDouble();
-					
-					System.out.println(rand);
-					
-					if (((double)pozastavenaData / (double)propustnostHrany) > chybovostHrany) 
-					if (rand < 0.5) {
-						ubytek = (int)Math.ceil(pozastavenaData/2.0); 
-						System.out.println("-Pøetížení!-  Pùlka dat se musí poslat znovu (" + ubytek + ").");
-						ztratovost += ubytek;
-						posliData(vterina, ubytek, dijkstra);
-						
-						System.out.println("\n--- vterina " + vterina + " ---");
-						poslano = pozastavenaData - ubytek;		
-					}
-					
-					System.out.println("Z vrcholu " + prvni.getId() + " do vrcholu " + druhy.getId() + " bylo posláno " + poslano + " dat.");
-					//poslat data
-					prvni.stack.smazatData(pozastavenaData);
-					System.out.println(pozastavenaData + " dat bylo smazáno ze stacku vrcholu " + prvni.getId() + ".");
-					break;
-				}
-				
-				rand = r.nextDouble();
-				
-				System.out.println(rand);
-				
-				poslano = propustnostHrany;
-				if (chybovostHrany != 1.0)
-				if (rand < 0.5) {
-					ubytek = (int)Math.ceil(propustnostHrany/2.0); 
-					System.out.println("-Pøetížení!-  Pùlka dat se musí poslat znovu (" + ubytek + ").");
-					ztratovost += ubytek;
-					posliData(vterina, ubytek, dijkstra);
-					
-					System.out.println("\n--- vterina " + vterina + " ---");
-					poslano = propustnostHrany - ubytek;	
+					if (((double)data / (double)propustnostHrany) > chybovostHrany) 
+						if (rand < 0.5) {
+							ubytek = (int)Math.ceil(data/2.0);
+							System.out.println("-Pøetížení!-  Pùlka dat se musí poslat znovu (" + ubytek + ").");
+							bw.write("-Pøetížení!-  Pùlka dat se musí poslat znovu (" + ubytek + ").");
+							bw.newLine();
+							ztratovost += ubytek;
+							posliData(vterina, ubytek, dijkstra);
+
+							System.out.println("\n--- vterina " + vterina + " ---");
+							bw.write("--- vterina " + vterina + " ---");
+							bw.newLine();
+							bw.newLine();
+							data = data - ubytek;
+
+						}
+
+					System.out.println("Z vrcholu " + prvni.getId() + " do vrcholu " + druhy.getId() + " bylo posláno " + data + " dat.");
+					bw.write("Z vrcholu " + prvni.getId() + " do vrcholu " + druhy.getId() + " bylo posláno " + data + " dat.");
+					bw.newLine();
+					//poslat data dal
+
 				}
 
-				System.out.println("Z vrcholu " + prvni.getId() + " do vrcholu " + druhy.getId() + " boly posláno " + poslano + " dat.");
-				//poslat data
-				prvni.stack.smazatData(propustnostHrany);
-				System.out.println(propustnostHrany + " dat bylo smazáno ze stacku vrcholu " + prvni.getId() + ".");
-				pozastavenaData -= propustnostHrany;	
+				else{
+					int pozastavenaData = data - propustnostHrany;
+					poslano = propustnostHrany;
+					rand = r.nextDouble();
+
+					if (chybovostHrany != 1.0)
+						if (rand < 0.5) {
+							ubytek = (int)Math.ceil(propustnostHrany/2.0); 
+							System.out.println("-Pøetížení!-  Pùlka dat se musí poslat znovu (" + ubytek + ").");
+							bw.write("-Pøetížení!-  Pùlka dat se musí poslat znovu (" + ubytek + ").");
+							bw.newLine();
+							ztratovost += ubytek;
+							posliData(vterina, ubytek, dijkstra);
+
+							System.out.println("\n--- vterina " + vterina + " ---");
+							bw.write("--- vterina " + vterina + " ---");
+							bw.newLine();
+							bw.newLine();
+							poslano = propustnostHrany - ubytek;
+						}
+					System.out.println("Z vrcholu " + prvni.getId() + " do vrcholu " + druhy.getId() + " bylo posláno " + poslano + " dat.");
+					bw.write("Z vrcholu " + prvni.getId() + " do vrcholu " + druhy.getId() + " bylo posláno " + poslano + " dat.");
+					bw.newLine();
+					//poslat data
+					if (!prvni.stack.pridatPamet(pozastavenaData)) {
+						System.out.println("Pamì vrcholu " + prvni.getId() + " pøetekla! Data se musí poslat znovu (" +  pozastavenaData + ").");
+						bw.write("Pamì vrcholu " + prvni.getId() + " pøetekla! Data se musí poslat znovu (" +  pozastavenaData + ").");
+						bw.newLine();
+						ztratovost += pozastavenaData;
+						posliData(vterina, pozastavenaData, dijkstra);
+						break;
+					}
+					else
+						System.out.println(pozastavenaData + " dat bylo uloženo do stacku vrcholu " + prvni.getId() + ".");
+					bw.write(pozastavenaData + " dat bylo uloženo do stacku vrcholu " + prvni.getId() + ".");
+					bw.newLine();
+
+					while(pozastavenaData != 0) {
+						vterina++;
+						System.out.println("\n--- vterina " + vterina + " ---");
+						bw.write("--- vterina " + vterina + " ---");
+						bw.newLine();
+						bw.newLine();
+
+						if (propustnostHrany > pozastavenaData) {
+							poslano = pozastavenaData;
+							rand = r.nextDouble();
+
+							if (((double)pozastavenaData / (double)propustnostHrany) > chybovostHrany) 
+								if (rand < 0.5) {
+									ubytek = (int)Math.ceil(pozastavenaData/2.0); 
+									System.out.println("-Pøetížení!-  Pùlka dat se musí poslat znovu (" + ubytek + ").");
+									bw.write("-Pøetížení!-  Pùlka dat se musí poslat znovu (" + ubytek + ").");
+									bw.newLine();
+									ztratovost += ubytek;
+									posliData(vterina, ubytek, dijkstra);
+
+									System.out.println("\n--- vterina " + vterina + " ---");
+									bw.write("--- vterina " + vterina + " ---");
+									bw.newLine();
+									bw.newLine();
+									poslano = pozastavenaData - ubytek;		
+								}
+
+							System.out.println("Z vrcholu " + prvni.getId() + " do vrcholu " + druhy.getId() + " bylo posláno " + poslano + " dat.");
+							bw.write("Z vrcholu " + prvni.getId() + " do vrcholu " + druhy.getId() + " bylo posláno " + poslano + " dat.");
+							bw.newLine();
+							//poslat data
+							prvni.stack.smazatData(pozastavenaData);
+							System.out.println(pozastavenaData + " dat bylo smazáno ze stacku vrcholu " + prvni.getId() + ".");
+							bw.write(pozastavenaData + " dat bylo smazáno ze stacku vrcholu " + prvni.getId() + ".");
+							bw.newLine();
+							break;
+						}
+
+						rand = r.nextDouble();
+
+						poslano = propustnostHrany;
+						if (chybovostHrany != 1.0)
+							if (rand < 0.5) {
+								ubytek = (int)Math.ceil(propustnostHrany/2.0); 
+								System.out.println("-Pøetížení!-  Pùlka dat se musí poslat znovu (" + ubytek + ").");
+								bw.write("-Pøetížení!-  Pùlka dat se musí poslat znovu (" + ubytek + ").");
+								bw.newLine();
+								ztratovost += ubytek;
+								posliData(vterina, ubytek, dijkstra);
+
+								System.out.println("\n--- vterina " + vterina + " ---");
+								bw.write("--- vterina " + vterina + " ---");
+								bw.newLine();
+								bw.newLine();
+								poslano = propustnostHrany - ubytek;	
+							}
+
+						System.out.println("Z vrcholu " + prvni.getId() + " do vrcholu " + druhy.getId() + " boly posláno " + poslano + " dat.");
+						bw.write("Z vrcholu " + prvni.getId() + " do vrcholu " + druhy.getId() + " boly posláno " + poslano + " dat.");
+						bw.newLine();
+						//poslat data
+						prvni.stack.smazatData(propustnostHrany);
+						System.out.println(propustnostHrany + " dat bylo smazáno ze stacku vrcholu " + prvni.getId() + ".");
+						bw.write(propustnostHrany + " dat bylo smazáno ze stacku vrcholu " + prvni.getId() + ".");
+						bw.newLine();
+						pozastavenaData -= propustnostHrany;	
+					}
+
 				}
-				
+
 			}
-			
+			bw.close();
+		}
+
+		catch (Exception e) {
+
+			System.err.println("Nepodaøilo se zapsat do souboru 'vstupTest.txt'.");
 		}
 	}
 
 	public int vypisZtratovost() {
-		System.out.println("\nZtrátovost je " + ztratovost);
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter("prubehSimulace.txt", true))) {
+			System.out.println("\nZtrátovost je " + ztratovost);
+			bw.newLine();
+			bw.write("Ztrátovost je " + ztratovost);
+			bw.newLine();
+			bw.newLine();
+			bw.close();
+		}
+		catch (Exception e) {
+
+			System.err.println("Nepodaøilo se zapsat do souboru 'vstupTest.txt'.");
+		}
 		return ztratovost;
+
 	}
-	
-	
+
+
 }
